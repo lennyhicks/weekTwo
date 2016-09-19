d738f40f55afc9a6b22f570dfadd06c9a70f9e38
Lenny Hicks <lenny.hicks@interapthq.com>
2016-09-19 16:41:03 -0400

    More jQuery

--- dayOne/js/jQuery3.js
+++ dayOne/js/jQuery3.js
@@ -1,7 +1,12 @@
 function myButton_Click() {
     alert("Clicked");
 }
+
 $(function() {
     //Wire up the click event handler
     $("#myButton").on("click", myButton_Click);
+
+    $("li:contains('mad')").on("click", function() {
+        $(this).hide();
+    });
 });
