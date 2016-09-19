d7a66d870f79053a0b9e42a925d587199fabfbd4
Lenny Hicks <lenny.hicks@interapthq.com>
2016-09-19 14:32:32 -0400

    Starting jQuery

--- dayOne/js/events.js
+++ dayOne/js/events.js
@@ -1,16 +1,47 @@
-function pageLoaded() {
-    alert("Hello World! I'm alive!");
-}
+    var randNumber = Math.floor(Math.random() * 10);
+    var wincount = 0;
 
-function buttonClick() {
-    alert("Hi There!");
-}
 
-function sayHello() {
-    var name = prompt("What's your name?");
-    alert("Hello, " + name + "!");
-}
+    function restartGame() {
+        randNumber = parseInt(Math.random() * 10) + 1;
+        startGame();
+    }
 
-function loadPage() {
-    document.location = "http://apple.com";
-}
+    function startGame() {
+        randNumber = parseInt(Math.random() * 10) + 1;
+        var guess = prompt("What is your Guess?");
+
+        guess *= 1;
+        while (guess != randNumber || guess != "quit") {
+            if (guess == "quit") {
+                alert("You gave up on the game. The correct answer was " + randNumber);
+                break;
+            } else if (guess != randNumber) {
+                guess = prompt("Guess again. You was incorrect");
+            } else if (guess = randNumber) {
+                wincount++;
+                alert("You won the game. Your win count is now " + wincount);
+                break;
+            }
+        }
+    }
+
+    function sayHello() {
+        var name = prompt("What's your name?");
+        alert("Hello, " + name + "!");
+    }
+
+    function doMath() {
+        var firstNum = prompt("What is your first Number?");
+        var param = prompt("What would you like to do? + - / *");
+        var secNum = prompt("What is your second Number?");
+        var result = eval(firstNum + param + secNum);
+        alert("The result of the equation you entered is " + result);
+    }
+
+
+    function doMathEquation() {
+        var equation = prompt("What is your Math Equation?");
+        var result = eval(equation);
+        alert("The result of the equation you entered is " + result);
+    }
